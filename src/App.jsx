124923bd46cb79d8f341d6fc92adeef90dfdd9f4
Nomesh vyas <nomesh9351962867@gmail.com>
2025-07-18 20:27:10 +0530

    NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import { div } from "framer-motion/client"
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Tech from "./components/Tech";
+import Works from "./components/Works";
+import Feedbacks from "./components/Feedbacks";
+import Contacts from "./components/Contacts";
+import StartsCanvas from "./components/StartsCanvas";
 
 const App = () => {
   return (
-    <div>
-      3D Developer Portfolio for Nomesh
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contacts />
+          <StartsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
 
   )
 }
